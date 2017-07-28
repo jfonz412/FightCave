21f4f67f88015bd3e2b511c1bd484d2eb7bc4acf
James Fonzone <jfonzone@outlook.com>
2017-07-27 21:38:35 -0400

    create and destroy goblin

---
+++ Assets/Scripts/GoblinDeath.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoblinDeath : MonoBehaviour {
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Weapon"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
