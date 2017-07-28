7ebca00f7b607936dfd80083bd0ce85031154458
James Fonzone <jfonzone@outlook.com>
2017-07-27 21:23:12 -0400

    syntax fix before branch split

--- Assets/Scripts/FireBoltCollision.cs
+++ Assets/Scripts/FireBoltCollision.cs
@@ -3,14 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FireBoltCollision : MonoBehaviour {
-    //private Collider collider;
-
-    void Start()
-    {
-        //collider = GetComponent<Collider>();
-    }
-
-
     void OnTriggerEnter(Collider other)
     {
         if (!other.gameObject.CompareTag("Player"))
