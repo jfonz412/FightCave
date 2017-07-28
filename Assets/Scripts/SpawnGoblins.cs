9b93aa8c6f4823b059f5f99fee9d7359e3e9fc19
James Fonzone <jfonzone@outlook.com>
2017-07-27 22:06:38 -0400

    goblin spawner

---
+++ Assets/Scripts/SpawnGoblins.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnGoblins : MonoBehaviour {
+    public GameObject monster;
+
+    public int amount;
+    public float delay;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(Spawn());
+    }
+
+    IEnumerator Spawn()
+    {
+        for(int i = 0; i < amount; i++)
+        {
+            Instantiate(monster, monster.transform.position, transform.rotation);
+            yield return new WaitForSeconds(delay);
+        }
+    }
+}
