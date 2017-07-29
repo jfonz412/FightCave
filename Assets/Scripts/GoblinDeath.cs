f051728167b29a6423015d2ceee2048cd7231a63
James Fonzone <jfonzone@outlook.com>
2017-07-28 23:08:29 -0400

    health script

--- Assets/Scripts/GoblinDeath.cs
+++ Assets/Scripts/GoblinDeath.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GoblinDeath : MonoBehaviour {
+    private Health health;
+
+    void Start()
+    {
+        health = GetComponent<Health>();
+    }
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Weapon"))
         {
-            Destroy(gameObject);
+            health.TakeDamage(1);
         }
     }
 }
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+    public int hp;// { get; set; }
+
+	// Update is called once per frame
+	void Update () {
+		if(hp <= 0)
+        {
+            Destroy(gameObject);
+        }
+	}
+
+    public void TakeDamage(int damage)
+    {
+        hp = hp - damage;
+    }
+}
