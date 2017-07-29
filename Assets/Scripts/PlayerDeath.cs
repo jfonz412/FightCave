958c03fcd33a2ac86974d744d83a9f2cdd82f5fe
James Fonzone <jfonzone@outlook.com>
2017-07-28 23:23:14 -0400

    player death

---
+++ Assets/Scripts/PlayerDeath.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeath : MonoBehaviour {
+    private Health health;
+
+    void Start()
+    {
+        health = GetComponent<Health>();
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            health.TakeDamage(1);
+        }
+    }
+}
