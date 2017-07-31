3347a4f9088aa1819e60600c724c78b76251b6ef
James Fonzone <jfonzone@outlook.com>
2017-07-31 00:04:46 -0400

    add notes to playercontroller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,11 +13,17 @@ public class PlayerController : MonoBehaviour {
 
     void ControlPlayer()
     {
-        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        //will be 1 or -1 depending on input
+        float moveHorizontal = Input.GetAxisRaw("Horizontal"); 
         float moveVertical = Input.GetAxisRaw("Vertical");
+
+        //create a location to move to, multiply it by speed and time in seconds
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        transform.Translate(movement * -speed * Time.deltaTime, Space.World);
-        if (movement != Vector3.zero)
+
+        //move player with time in seconds (as opposed to frames) relative to the world
+        transform.Translate(movement * -speed * Time.deltaTime, Space.World); // as opposed to Self
+
+        if (movement != Vector3.zero) //make sure player is moving before rotating
         {
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15F);
         }
