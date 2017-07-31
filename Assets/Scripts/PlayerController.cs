1bb99276764d1199bdd87202d74229d7f4c7eaa8
James Fonzone <jfonzone@outlook.com>
2017-07-30 23:55:05 -0400

    fix player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public int speed;
+
+    void Update()
+    {
+        ControlPlayer();
+    }
+
+
+    void ControlPlayer()
+    {
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        transform.Translate(movement * -speed * Time.deltaTime, Space.World);
+        if (movement != Vector3.zero)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15F);
+        }
+    }
+}
